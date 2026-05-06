b32aef7523ae1e7dd84c99e7d8fe8d503a081228
asadulla <normengliyevasadulla@gmail.com>
2026-05-06 17:33:24 +0500

    update eat method in Apple class to change output message

--- src/main/java/com/example/test/Apple.java
+++ src/main/java/com/example/test/Apple.java
@@ -1,8 +1,8 @@
 package com.example.test;
 
-// main branch
 public class Apple {
     void eat() {
-        System.out.println("eat apple in main");
+        System.out.println("Salom MAIN dan"); // Faqat shu qatorga e'tibor bering
     }
 }
+
