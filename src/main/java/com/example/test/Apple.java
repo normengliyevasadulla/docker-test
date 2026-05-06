70673fb7d203bf31b3c7ceee81925cd6a84bcc35
asadulla <normengliyevasadulla@gmail.com>
2026-05-06 17:39:12 +0500

    update eat method in Apple class to correct output message casing

--- src/main/java/com/example/test/Apple.java
+++ src/main/java/com/example/test/Apple.java
@@ -2,7 +2,7 @@ package com.example.test;
 
 public class Apple {
     void eat() {
-        System.out.println("Salom MAIN dan"); // Faqat shu qatorga e'tibor bering
+        System.out.println("Salom main dan"); // Faqat shu qatorga e'tibor bering
     }
 }
 
