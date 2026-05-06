1ec756d2a2fadcddb3d6d4642879af94f2518b2f
asadulla <normengliyevasadulla@gmail.com>
2026-05-06 17:27:54 +0500

    update eat method in Apple class to include branch context

--- src/main/java/com/example/test/Apple.java
+++ src/main/java/com/example/test/Apple.java
@@ -1,7 +1,8 @@
 package com.example.test;
 
+// main branch
 public class Apple {
     void eat() {
-        System.out.println("eat apple");
+        System.out.println("eat apple in main");
     }
 }
